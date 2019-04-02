48df2baa37ece065e8cc0472fcd773f48c9922ac
Dotsung <andy395023@gmail.com>
2019-04-02 21:12:42 +0900

    mobx설정, 로그인창

---
+++ front/src/Login.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { observable, action } from 'mobx'
+import { observer } from 'mobx-react'
+
+@observer
+class Login extends Component {
+  @observable email = ''
+  @observable passwords = ''
+
+  constructor(props) {
+    super(props)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  render() {
+    const { email, password } = this
+
+    return (
+      <div className="App">
+        <header>Login</header>
+          <input name="email" placeholder="Email" onChange={this.onChange} value={email} fluid />
+          <input
+            name="password"
+            type="password"
+            placeholder="Password"
+            onChange={this.onChange}
+            value={password}
+            fluid
+          />
+        <button onClick={this.onSubmit}>Submit</button>
+      </div>
+    );
+  }
+
+  @action.bound
+  onChange(event) {
+    const { name, value } = event.target;
+    this[name] = value;
+  }
+
+  onSubmit() {
+    const { email, password } = this
+    console.log('결과확인 : ', email, password);
+  }
+}
+
+export default Login;
