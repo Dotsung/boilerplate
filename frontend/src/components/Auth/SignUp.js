a3e0b5219ac41390bd31bbb32e6232bd95e43d0e
Dotsung <andy395023@gmail.com>
2019-04-09 20:30:37 +0900

    백엔드 추가

---
+++ frontend/src/components/Auth/SignUp.js
@@ -0,0 +1,69 @@
+// @flow
+import React from 'react';
+import { observable, action } from 'mobx'
+import { observer, inject } from 'mobx-react'
+import db from 'shared/firebaseInit';
+import { SignUpContainer, Form, StyledH1, SocialContainer, SocialA, StyledSpan, StyledInput, StyledButton } from 'components/Auth/Style';
+
+@inject("authToggle")
+@observer
+class SignUp extends React.Component {
+  @observable userName = ''
+  @observable email = ''
+  @observable passwords = ''
+
+  render() {
+    const { userName, email, password } = this
+    const { rightPanelActive, Toggle } = this.props.authToggle;
+
+    return(
+      <SignUpContainer rightPanelActive={rightPanelActive}>
+        <Form>
+          <StyledH1>Create Account</StyledH1>
+          <SocialContainer>
+            <SocialA><i className="fab fa-facebook-f"></i></SocialA>
+            <SocialA><i className="fab fa-google-plus-g"></i></SocialA>
+            <SocialA><i className="fab fa-linkedin-in"></i></SocialA>
+          </SocialContainer>
+          <StyledSpan>or use your email for registration</StyledSpan>
+          <StyledInput name="userName" placeholder="Name" onChange={this.onChange} value={userName} onKeyPress={this.onPress} />
+          <StyledInput name="email" placeholder="Email" onChange={this.onChange} value={email} onKeyPress={this.onPress} />
+          <StyledInput
+            name="password"
+            type="password"
+            placeholder="Password"
+            onChange={this.onChange}
+            value={password}
+            onKeyPress={this.onPress} 
+          />
+          <StyledButton onClick={this.onSubmit}>Sign Up</StyledButton>
+        </Form>
+      </SignUpContainer>
+    )
+  }
+
+  @action.bound
+  onPress(f){
+    if(f.key == 'Enter'){
+      this.onSubmit();
+    }
+  }
+  
+  @action.bound
+  onChange(event) {
+    const { name, value } = event.target;
+    this[name] = value;
+  }
+
+  @action.bound
+  onSubmit() {
+    const { userName, email, password } = this
+    const userRef = db.collection('users').add({
+      userName: userName,
+      email: email,
+      password: password
+    });
+  }
+}
+
+export default SignUp;
---
+++ frontend/src/pages/Auth.js
@@ -0,0 +1,29 @@
+// @flow
+import React from 'react';
+import { observable, action } from 'mobx'
+import { observer, inject } from 'mobx-react'
+import styled from 'styled-components';
+import SignUp from 'components/Auth/SignUp';
+import SignIn from 'components/Auth/SignIn';
+import Overlay from 'components/Auth/Overlay';
+import { Div, Container} from 'components/Auth/Style';
+
+
+@inject("testStore")
+@observer
+class Auth extends React.Component {
+  render() {
+    return (
+      <Div>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossOrigin="anonymous"></link>
+        <Container>
+            <SignUp />
+            <SignIn />
+            <Overlay />
+        </Container>
+      </Div>
+    );
+  }
+}
+
+export default Auth;
